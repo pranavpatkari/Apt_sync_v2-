a5cefa31596c0017bbabb09880d57d7f406198ce
pranavpatkari <pranavpatkarixyz@gmail.com>
2026-04-23 19:18:17 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyClt8aVCOPc-vxMdJ-LAJxY5C7kTieVteM",
-  authDomain: "projectawstl.firebaseapp.com",
-  projectId: "projectawstl",
-  storageBucket: "projectawstl.firebasestorage.app",
+  authDomain: "projectawstlApt.firebaseapp.com",
+  projectId: "projectawstlApt",
+  storageBucket: "projectawstlApt.firebasestorage.app",
   messagingSenderId: "710136480889",
   appId: "1:710136480889:web:89ed8283280a70a9f900a8",
   measurementId: "G-8JJ5HSZM8E"
