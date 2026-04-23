ab46170756bf1704bfac1262ebf8aee9ff02c949
pranavpatkari <pranavpatkarixyz@gmail.com>
2026-04-23 19:23:51 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClt8aVCOPc-vxMdJ-LAJxY5C7kTieVteM",
-  authDomain: "projectawstlApt.firebaseapp.com",
-  projectId: "projectawstlApt",
-  storageBucket: "projectawstlApt.firebasestorage.app",
-  messagingSenderId: "710136480889",
-  appId: "1:710136480889:web:89ed8283280a70a9f900a8",
-  measurementId: "G-8JJ5HSZM8E"
+
+  apiKey: "AIzaSyAr72OFdHeQwrrfeWWp1DhgVz4ZFcmoDjY",
+
+  authDomain: "projectawstlapt.firebaseapp.com",
+
+  projectId: "projectawstlapt",
+
+  storageBucket: "projectawstlapt.firebasestorage.app",
+
+  messagingSenderId: "1051010128303",
+
+  appId: "1:1051010128303:web:cb77b471d9228538d6775a",
+
+  measurementId: "G-L1MGMSNP83"
+
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 export const db  = getFirestore(app);
